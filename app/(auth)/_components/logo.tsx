d31c61146d53cd781c5d5fb3f9c6c8ab3fe802a9
Christian Halim <chris.richard.halim@gmail.com>
2024-01-14 22:04:39 +0700

    add db

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,34 @@
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+    return (
+        <div className="flex flex-col items-center gap-y-4">
+            <div className="bg-white rounded-full p-1">
+                <Image src="/spooky.svg" 
+                    alt="Atelier"
+                    height="80"
+                    width="80"
+                />
+            </div>
+            <div className={cn(
+                "flex flex-col items-center",
+                font.className
+                )}>
+                <p className="text-xl font-semibold">
+                    Atelier
+                </p>
+                <p className="text-sm text-muted-foreground">
+                    Share new things!
+                </p>
+            </div>
+        </div>
+    )
+}
