1d74011b81f85c720ed80c66890b89d24f932109
Christian Halim <chris.richard.halim@gmail.com>
2024-01-20 18:56:42 +0700

    Add error handling when failed to update on prisma (just in case)
    But, this only works if the process is not inside a try catch block. Since the exception will be catched instead of returning null / false;
    
    https://github.com/prisma/prisma/issues/10142#issuecomment-1693864308

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 declare global {
     var prisma: PrismaClient | undefined;
@@ -7,3 +7,18 @@ declare global {
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+export async function recoverFromNotFound<A>(
+    promise: Promise<A>,
+  ): Promise<A | null> {
+    try {
+      return await promise;
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          return null;
+        }
+      }
+      throw e;
+    }
+  }
