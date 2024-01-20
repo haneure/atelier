944c21760c97f2d06d2e85475fba5dfc17ed0a76
Christian Halim <chris.richard.halim@gmail.com>
2024-01-20 21:11:29 +0700

    add Keys settings template

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -5,6 +5,7 @@ import { useTransition } from "react";
 
 import { updateStream } from "@/actions/stream";
 import { Switch } from "@/components/ui/switch";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly"
 
@@ -46,5 +47,11 @@ export const ToggleCard = ({
                 </div>
             </div>    
         </div>
-    )
-}
+    );
+};
+
+export const ToggleCardSkeleton = () => {
+    return (
+        <Skeleton className="rounded-xl p-10 w-full" />
+    );
+};
