48f2432afba05f0e3a36080e714f596a9c09ecc8
Christian Halim <chris.richard.halim@gmail.com>
2024-01-20 15:08:06 +0700

    show followed list and improve recommended list

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -2,17 +2,22 @@ import { getRecommended } from '@/lib/recommended-service';
 import { Recommended, RecommendedSkeleton } from './recommended';
 import { Toggle, ToggleSkeleton } from './toggle';
 import { Wrapper } from './wrapper'
+import { getFollowedUsers } from '@/lib/follow-service';
+import { Following, FollowingSkeleton } from './following';
 
 export const Sidebar = async () => {
-    // fetch followed
     // fetch recommended
     const recommended = await getRecommended();
+
+     // fetch followed
+    const following = await getFollowedUsers();
 
   return (
     <Wrapper>
         <Toggle />
         <div className="space-y-4 pt-4 lg:pt-0">
-          <Recommended data={recommended}/>
+          <Following data={following} />
+          <Recommended data={recommended} />
         </div>
     </Wrapper>
   );
@@ -22,6 +27,7 @@ export const SidebarSkeleton = () => {
   return (
     <aside className='fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50'>
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   )
