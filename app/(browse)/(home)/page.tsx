7bb20d56c55af21637bc67238583b1534a7b7302
Christian Halim <chris.richard.halim@gmail.com>
2024-01-23 20:35:28 +0700

    Add Home page
    skeleton not working,
    not sure why

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import { Suspense } from "react";
+import { Results, ResultsSkeleton } from "./_components/results";
 
 export default function Home() {
     return (
-        <div className="flex flex-col gap-y-4">
-            <h1>Homepage</h1>
+        <div className="h-full p-8 max-w-screen-2xl mx-auto">
+            <Suspense fallback={<ResultsSkeleton />}>
+                <Results />
+            </Suspense>
         </div>
     );
-}
+};
--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -13,8 +13,6 @@ interface RecommendedProps {
 export const Recommended = ({ data, }: RecommendedProps) => {
     const { collapsed } = useSidebar((state) => state);
 
-    console.log(data.length);
-
     const showLabel = !collapsed && data.length > 0;
 
     return (
